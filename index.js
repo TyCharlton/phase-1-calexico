6697b67b666b63942345a5198891e506abe596f3
TylerCharlton <TyCharlton1993@gmail.com>
2023-12-20 18:58:10 -0500

    Completes deliv3

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Write your code here...
+
+//Global variables
+const URL = "http://localhost:3000/menu"
+
+
+// DOM selectors
+const menuItems = document.querySelector("#menu-items")
+const dishImage = document.querySelector("#dish-image")
+const dishName = document.querySelector("#dish-name")
+const dishDescription = document.querySelector("#dish-description")
+const dishPrice = document.querySelector("#dish-price")
+
+
+
+// Fetch functions
+function getAllDishes(url){
+    fetch(url)
+     .then(res => res.json())
+     .then(dishArr => {
+        dishArr.forEach(dishObj => renderMenu(dishObj))
+        renderDetails(dishArr[0]);
+    })
+}
+
+
+// Render functions
+function renderMenu(dishObj){
+    const dish = document.createElement("span")
+    dish.textContent = dishObj.name
+    dish.addEventListener('click', () => renderDetails(dishObj))
+    // console.log(dish)
+    menuItems.append(dish);
+
+}
+
+function renderDetails(dishObj){
+    console.log(dishObj)
+    dishImage.src = dishObj.image
+    dishName.textContent = dishObj.name
+    dishDescription.textContent = dishObj.description
+    dishPrice.textContent = `$${dishObj.price}`   
+}
+
+
+// Even Listeners & Handlers
+
+
+// Initializer
+getAllDishes(URL)
+
+
