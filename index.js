7709d0d0dadd61704d42b2fbb6b16e8320c82e0e
TylerCharlton <TyCharlton1993@gmail.com>
2023-12-20 19:53:26 -0500

    completed lab

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 //Global variables
 const URL = "http://localhost:3000/menu"
+let currentDish
 
 
 // DOM selectors
@@ -10,6 +11,8 @@ const dishImage = document.querySelector("#dish-image")
 const dishName = document.querySelector("#dish-name")
 const dishDescription = document.querySelector("#dish-description")
 const dishPrice = document.querySelector("#dish-price")
+const cartForm = document.querySelector("#cart-form")
+const numInCart = document.querySelector("#cart-amount")
 
 
 
@@ -40,13 +43,29 @@ function renderDetails(dishObj){
     dishName.textContent = dishObj.name
     dishDescription.textContent = dishObj.description
     dishPrice.textContent = `$${dishObj.price}`   
+    numInCart.textContent = dishObj.number_in_bag
+    
 }
 
 
 // Even Listeners & Handlers
+cartForm.addEventListener("submit", addToCart)  // addEventListener passes event
+                                                // to the invoked handler callback function
+
+function addToCart(e){
+    e.preventDefault()
+    const numToAdd = Number(e.target.amount.value)
+    console.logNumber = Number(e.target.amount.value)
+    const currNum = Number(numInCart.textContent)
+    numInCart.textContent = currNum + numToAdd
+    currentDish.number_in_bag += numToAdd;
+    renderDetails(currentDetails)
+    e.target.reset()
+    }
 
 
 // Initializer
 getAllDishes(URL)
 
 
+//Check for defer attribute.
